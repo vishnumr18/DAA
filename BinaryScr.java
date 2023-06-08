89fb463ef560941fa7527f24fae11b467d7aac71
VISHNU M R <111166547+vishnumr18@users.noreply.github.com>
2023-06-08 22:43:18 +0530

    debugged

--- BinaryScr.java
+++ BinaryScr.java
@@ -1,35 +1,15 @@
 import java.util.*;
 
 public class BinaryScr {
-  public static int  binaryScr(int arr[], int key) {
-    int low = 0;
-    int high = arr.length - 1;
-    
-    while (low <= high) {
-      int middle = (low + high) / 2;
-      int currentVal = arr[middle];
 
-      if (currentVal == key) {
-        return middle + 1;
-      }  if (currentVal < middle) {
-        high = middle - 1;
-      } else {
-        high = middle + 1;
-      }
-    }
-    // System.out.println("The element is not found!!!!");
-    
-  }
 
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-
     System.out.println("Enter the number of elements: ");
     int n = in.nextInt();
 
     int arr[] = new int[n];
-    System.out.println("Enter the elements: ");
-
+    System.out.format("Enter %d elements: ",n);
     for (int i = 0; i < n; i++) {
       arr[i] = in.nextInt();
     }
@@ -37,9 +17,31 @@ public class BinaryScr {
     System.out.println("Enter the key to be searched: ");
     int key = in.nextInt();
 
+    int res = binScr(arr, key);
+    // System.out.println(res);
+    if(res != -1){
+      System.out.println("The element is found at positon: "+(res+1));
+    }else{
+      System.out.println("The elenent is not found!!!");
+    }
+  }
+  
+  public static int binScr(int arr[], int key) {
     
-    // int print = binaryScr(arr, key);
-    System.out.println(binaryScr(arr, key));
-    
+    int low = 0;
+    int high = arr.length - 1;
+
+    while (low <= high) {
+      int mid = (low + high) / 2;
+      if (arr[mid] == key) {
+        return mid;
+      } else if (arr[mid] > key) {
+        
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+    return -1;
   }
 }
